e65f22b2fe48adc5a793a9f41cf82fdfdd238d84
frowhy <frowhy@gmail.com>
2017-03-15 00:35:51 +0800

    修复问题

--- app/src/main/java/com/frowhy/wake/MainActivity.java
+++ app/src/main/java/com/frowhy/wake/MainActivity.java
@@ -8,16 +8,17 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.frowhy.wake.model.Schema;
 import com.google.gson.Gson;
 
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -29,6 +30,27 @@ public class MainActivity extends Activity {
 
     private View mBtnTest;
     private PackageManager packageManager;
+    private boolean checkRoot = false;
+    private int count = 0;
+
+    protected int execRootCmdSilent(String paramString) {
+        try {
+            Process localProcess = Runtime.getRuntime().exec("su");
+            Object localObject = localProcess.getOutputStream();
+            DataOutputStream localDataOutputStream = new DataOutputStream(
+                    (OutputStream) localObject);
+            String str = String.valueOf(paramString);
+            localObject = str + "\n";
+            localDataOutputStream.writeBytes((String) localObject);
+            localDataOutputStream.flush();
+            localDataOutputStream.writeBytes("exit\n");
+            localDataOutputStream.flush();
+            localProcess.waitFor();
+            return localProcess.exitValue();
+        } catch (Exception ignored) {
+            return -1;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,7 +81,6 @@ public class MainActivity extends Activity {
 
             String jsonStr = getLocalJson();
 
-            Log.d("----------------", jsonStr);
             Gson gson = new Gson();
 
             Schema schemas = gson.fromJson(jsonStr, Schema.class);
@@ -118,7 +139,7 @@ public class MainActivity extends Activity {
         });
     }
 
-    private void handle(Uri uri) {
+    private void handle(final Uri uri) {
         /* 该Schema存在 */
         if (mSp.contains(mSchema)) {
 
@@ -136,22 +157,52 @@ public class MainActivity extends Activity {
 
                         packageInfo = packageManager.getPackageInfo(packageName, 0);
                         if (!packageInfo.applicationInfo.enabled) {
-
-                            Process process = Runtime.getRuntime().exec("su");
-                            DataOutputStream os = new DataOutputStream(process.getOutputStream());
-                            os.writeBytes("pm enable " + packageName + "\n");
-                            os.writeBytes("exit\n");
-                            os.flush();
+                            count++;
+                            checkRoot = 0 == execRootCmdSilent("pm enable " + packageName);
                         }
                     } catch (PackageManager.NameNotFoundException ignored) {
-                    } catch (IOException e) {
-                        e.printStackTrace();
                     }
                 }
-            }
-        }
 
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        startActivity(intent);
+                if (count == 0) {
+                    Toast.makeText(this, "所需APP未被冻结,重新打开,请勿选择唤醒", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    if (checkRoot) {
+                        Toast.makeText(this, "已唤醒所需APP,正在打开,请等待...", Toast.LENGTH_SHORT).show();
+
+                        new Handler().postDelayed(new Runnable() {
+                            public void run() {
+
+                                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }, 1000);
+                    } else {
+                        Toast.makeText(this, "未获取ROOT权限,重新打开,请唤醒并授权", Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            } else {
+                Toast.makeText(this, "未记录该APP,请反馈Schema以及PackageName", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+                finish();
+            }
+        } else {
+            Toast.makeText(this, "未记录该APP,请反馈Schema以及PackageName", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+            finish();
+        }
     }
 }
